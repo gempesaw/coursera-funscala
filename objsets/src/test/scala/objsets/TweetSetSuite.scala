da578cb1723f7c345915a1762b590e64df10ca57
Daniel Gempesaw <gempesaw@gmail.com>
2013-04-24 00:59:33 -0400

    Finish week 3

--- objsets/src/test/scala/objsets/TweetSetSuite.scala
+++ objsets/src/test/scala/objsets/TweetSetSuite.scala
@@ -5,6 +5,8 @@ import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
+import TweetReader._
+
 @RunWith(classOf[JUnitRunner])
 class TweetSetSuite extends FunSuite {
   trait TestSets {
@@ -16,6 +18,15 @@ class TweetSetSuite extends FunSuite {
     val set4c = set3.incl(c)
     val set4d = set3.incl(d)
     val set5 = set4c.incl(d)
+
+    val leastLeft = new Tweet("a", "a tweet", 1)
+    val leastRight = new Tweet("z", "z tweet", 1)
+    val mostMiddle = new Tweet("m", "m tweet", 100)
+    val leastMiddle = new Tweet("m", "m tweet", 1)
+    val mostRight = new Tweet("z", "z tweet", 100)
+    val setMostMiddle = new NonEmpty(mostMiddle, new NonEmpty(leastLeft, new Empty, new Empty), new NonEmpty(leastRight, new Empty, new Empty))
+    val setMostLeft = new NonEmpty(leastMiddle, new NonEmpty(c, new Empty, new Empty), new NonEmpty(leastRight, new Empty, new Empty))
+    val setMostRight = new NonEmpty(leastMiddle, new NonEmpty(c, new Empty, new Empty), new NonEmpty(mostRight, new Empty, new Empty))
   }
 
   def asSet(tweets: TweetSet): Set[Tweet] = {
@@ -44,6 +55,15 @@ class TweetSetSuite extends FunSuite {
     }
   }
 
+  test("filter: various") {
+    new TestSets {
+      assert(size(set5.filter(tw => tw.user != "a")) === 3)
+      assert(size(set5.filter(tw => true)) === 4)
+      assert(size(set5.filter(tw => tw.retweets > 0)) === 4)
+      assert(size(set5.filter(tw => tw.retweets < 0)) === 0)
+    }
+  }
+
   test("union: set4c and set4d") {
     new TestSets {
       assert(size(set4c.union(set4d)) === 4)
@@ -62,9 +82,44 @@ class TweetSetSuite extends FunSuite {
     }
   }
 
+  test("union: chains!") {
+    new TestSets{
+      assert(size(set2 union set3 union set4c union set4d union set5) === 4)
+    }
+  }
+
+  test("union: duplicates") {
+    new TestSets{
+      assert(size(set1 union set1) === size(set1))
+      assert(size(set5 union set5) === size(set5))
+    }
+  }
+
+  test("most retweets: head of tree") {
+    new TestSets{
+      val tweet = setMostMiddle.mostRetweeted
+      assert(tweet === mostMiddle)
+    }
+  }
+
+  test("most retweets: left branch") {
+    new TestSets{
+      val tweet = setMostLeft.mostRetweeted
+      assert(tweet === c)
+    }
+  }
+
+  test("most retweets: right branch") {
+    new TestSets{
+      val tweet = setMostRight.mostRetweeted
+      assert(tweet === mostRight)
+    }
+  }
+
   test("descending: set5") {
     new TestSets {
       val trends = set5.descendingByRetweet
+
       assert(!trends.isEmpty)
       assert(trends.head.user == "a" || trends.head.user == "b")
     }
