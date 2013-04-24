aeac55c3890abfaca78c2cb59e55bd8f4b9eb88d
Daniel Gempesaw <gempesaw@gmail.com>
2013-04-24 01:06:15 -0400

    Undo tweet output modification

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -8,9 +8,8 @@ import TweetReader._
   */
 class Tweet(val user: String, val text: String, val retweets: Int) {
   override def toString: String =
-  //   "User: " + user + "\n" +
-  // "Text: " + text +
-  " [" + retweets + "]"
+    "User: " + user + "\n" +
+    "Text: " + text + " [" + retweets + "]"
 }
 
 /**
