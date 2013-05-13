1a174e95f3afd6099385fc125bbfff584a50e6a2
Daniel Gempesaw <gempesaw@gmail.com>
2013-05-13 00:03:42 -0400

    Week7: halfway...

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -12,30 +12,31 @@ class BloxorzSuite extends FunSuite {
 
   trait SolutionChecker extends GameDef with Solver with StringParserTerrain {
     /**
-     * This method applies a list of moves `ls` to the block at position
-     * `startPos`. This can be used to verify if a certain list of moves
-     * is a valid solution, i.e. leads to the goal.
-     */
+      * This method applies a list of moves `ls` to the block at position
+      * `startPos`. This can be used to verify if a certain list of moves
+      * is a valid solution, i.e. leads to the goal.
+      */
     def solve(ls: List[Move]): Block =
-      ls.foldLeft(startBlock) { case (block, move) => move match {
-        case Left => block.left
-        case Right => block.right
-        case Up => block.up
-        case Down => block.down
+      ls.foldLeft(startBlock) {
+        case (block, move) => move match {
+          case Left => block.left
+          case Right => block.right
+          case Up => block.up
+          case Down => block.down
+        }
       }
-    }
   }
 
   trait Level1 extends SolutionChecker {
-      /* terrain for level 1*/
+    /* terrain for level 1*/
 
     val level =
-    """ooo-------
-      |oSoooo----
-      |ooooooooo-
-      |-ooooooooo
-      |-----ooToo
-      |------ooo-""".stripMargin
+      """ooo-------
+        |oSoooo----
+        |ooooooooo-
+        |-ooooooooo
+        |-----ooToo
+        |------ooo-""".stripMargin
 
     val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
   }
@@ -43,13 +44,39 @@ class BloxorzSuite extends FunSuite {
   test("terrain function level 1") {
     new Level1 {
       assert(terrain(Pos(0,0)), "0,0")
+      assert(terrain(Pos(4,9)), "4,9")
+      assert(!terrain(Pos(4,10)), "4,10")
       assert(!terrain(Pos(4,11)), "4,11")
+      assert(!terrain(Pos(0,5)), "0,5")
+      assert(!terrain(Pos(5,0)), "5,0")
     }
   }
 
   test("findChar level 1") {
     new Level1 {
-      assert(startPos == Pos(1,1))
+      assert(startPos === Pos(1,1))
+      assert(goal === Pos(4,7))
+    }
+  }
+
+  test("nwh") {
+    new Level1 {
+      assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet ===
+        Set((Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))))
+    }
+  }
+
+  test("nno") {
+    new Level1 {
+      assert(newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      ) === Set((Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))).toStream)
     }
   }
 
